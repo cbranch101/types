e3878588ef74adee6a45023bd9fb729a161e1dfc
Clay Branch <clay.branch@gmail.com>
2023-02-28 08:56:55 -0700

    chore: basic repo structure

---
+++ src/base.test.js
@@ -0,0 +1,6 @@
+import wrapped from "./index";
+
+console.log(wrapped);
+test("A basic test", () => {
+  expect(1).toEqual(1);
+});
---
+++ src/index.js
@@ -0,0 +1,9 @@
+export const foo = "test";
+export const boo = "biz";
+
+const wrapped = {
+  foo,
+  boo,
+};
+
+export default wrapped;
